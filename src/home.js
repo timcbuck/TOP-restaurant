46383e9f308d3230bddda40f99f3e7178fb7e370
timcbuck <timcbuck.94@gmail.com>
2024-06-04 22:11:24 +0930

    Add menu content and module

--- src/home.js
+++ src/home.js
@@ -8,23 +8,29 @@ const loadHomePage = () => {
     content.innerHTML = '';
 
     // Create new elements to go inside content
+    const homeContent = document.createElement('div');
     const contentLeft = document.createElement('div');
     const contentRight = document.createElement('div');
     const contentImage = document.createElement('img');
 
     // Populate content
+    homeContent.setAttribute('id', 'home-content');
     contentLeft.innerHTML = `<h1>Ora Thai Restaurant</h1>
     <h2>Sawasdee</h2>
             <p>This is where culinary innovation meets the timeless charm of lunch and dinner classics. Our menu, a treasure trove of gourmet Thai cuisine, delicious cocktails, and the best Pad Thai in Adelaide, promises an unrivaled dining experience.</p>
-            <p>Savor the flavors of Thailand from decadent tom yum soup to savory spring rolls, each dish at Ora Thai is crafted to perfection, making us the go-to spot for weekend dinner. Whether you're indulging in a family-friendly group or seeking the perfect dinner spot for a Friday night, Ora Thai's unique menu caters to all.</p>`
+            <p>Savor the flavors of Thailand from decadent tom yum soup to savory spring rolls, each dish at Ora Thai is crafted to perfection, making us the go-to spot for weekend dinner.</p>
+            <p>Whether you're indulging in a family-friendly group or seeking the perfect dinner spot for a Friday night, Ora Thai's unique menu caters to all.</p>
+            <button id="btn-reservation">Make a reservation</button>
+            `
     contentLeft.setAttribute('id', 'content-left');
     contentImage.src = Spices;
     contentImage.setAttribute('id', 'content-image');
 
     // Append content together
     contentRight.appendChild(contentImage);
-    content.appendChild(contentLeft);
-    content.appendChild(contentRight);
+    homeContent.appendChild(contentLeft);
+    homeContent.appendChild(contentRight);
+    content.appendChild(homeContent);
 };
 
 export default loadHomePage;
--- src/menu.js
+++ src/menu.js
@@ -6,11 +6,41 @@ const loadMenuPage = () => {
     content.innerHTML = '';
 
     // Create new elements to go inside content
-    const contentLeft = document.createElement('h1');
-    contentLeft.textContent = 'MENU';
+    const menuContent = document.createElement('div');
+    menuContent.innerHTML = `
+    <div id="menu-content">
+        <div id="lunch-container">
+            <div id="lunch-header-container">
+                <h1>Lunch</h1>
+                <div id="lunch-image"></div>
+            </div>
+            <div id="lunch-items-container">
+                <ul>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                </ul>
+            </div>
+        </div>
+        <div id="dinner-container">
+            <div id="dinner-items-container">
+                <ul>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                    <li>Pad thai</li>
+                </ul>
+            </div>
+            <div id="dinner-header-container">
+                <h1>Dinner</h1>
+                <div id="dinner-image"></div>
+            </div>
+        </div>
+    </div>`
 
     // Append content together
-    content.appendChild(contentLeft);
+    content.appendChild(menuContent);
     
 }
 
